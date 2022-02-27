eaa76a223997081f6bf97ace2b0c03758fc40562
Cici Chen <iccchen@bu.edu>
2022-02-27 17:14:12 -0500

    convertvert html file to js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Map from './map';
 
 function App() {
-  var perf =require('./index.html');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <iframe src={perf }></iframe>   /* like this */
-        <p>
-          Freelance Beach Cleanup!
-        </p>
-      </header>
+        <Map style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}}></Map>
+<h1>hello</h1>
     </div>
   );
 }
