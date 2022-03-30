eb0d0ead33dc2e99628874b76e459ff9ebe947e0
Sam Leach <samleach@Sams-MacBook-Pro.local>
2022-03-30 14:14:41 -0400

    pwa tweaks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import MapComponent from './map/map.js';
 import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
-  .then((reg) => console.log('service worker registered', reg))
-  .catch((err) => console.log('service worker not registered', err))
-}
+// if('serviceWorker' in navigator){
+//   navigator.serviceWorker.register('/sw.js')
+//   .then((reg) => console.log('service worker registered', reg))
+//   .catch((err) => console.log('service worker not registered', err))
+// }
 function App() {
   return (
     <div className="App">
