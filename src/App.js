78c8c7fe0f92c1deda6bca392ad7016204b5af6c
Cici Chen <iccchen@bu.edu>
2022-04-11 14:14:01 -0400

    last try :(

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MapComponent from './map/map.js';
 import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("mapbox-gl/dist/mapbox-gl-csp-worker").default;
+// mapboxgl.workerClass = require("mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 function App() {
   return (
