bea2ae25efd067fedaef9dc02cc61b067407a362
Cici Chen <iccchen@bu.edu>
2022-04-14 00:35:22 -0400

    UI for map page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import MapComponent from './map/map.js';
 import mapboxgl from 'mapbox-gl';
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("mapbox-gl/dist/mapbox-gl-csp-worker").default;
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 function App() {
   return (
