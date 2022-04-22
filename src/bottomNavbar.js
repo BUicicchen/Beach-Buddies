365be1eee9c0ea93bc62e78478f0ff3cc2ee2c05
Cici Chen <iccchen@bu.edu>
2022-04-22 17:59:35 -0400

    modified logo

--- src/bottomNavbar.js
+++ src/bottomNavbar.js
@@ -8,8 +8,8 @@ const BottomNavbar = () => {
     return (
         <Box sx={{ flexGrow: 1, height: '70px', bgcolor: "#355598" }} position="static" >
             <Toolbar style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                {/* <img src={logo} style={{width:210, marginTop: -56, transform:`rotate(${7}deg)`}} /> */}
-                <img src={logo} style={{width:300, marginTop: -110, transform:`rotate(${7}deg)`}} />
+                <img src={logo} style={{width:210, marginTop: -56, transform:`rotate(${7}deg)`}} />
+                {/* <img src={logo} style={{width:300, marginTop: -110, transform:`rotate(${7}deg)`}} /> */}
                 {/* <img src={logo} style={{width:170, marginTop: -75}} /> */}
             </Toolbar>
         </Box>
