398b284699793bf5797646f32ba1df6aacc5d871
Cici Chen <iccchen@bu.edu>
2022-04-22 13:34:49 -0400

    new icons

--- src/bottomNavbar.js
+++ src/bottomNavbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import logo from './public/assets/logo.png';
+import logo from './public/assets/new_BB_logo.svg';
 
 const BottomNavbar = () => {
     return (
         <Box sx={{ flexGrow: 1, height: '70px', bgcolor: "#355598" }} position="static" >
             <Toolbar style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                {/* <img src={logo} style={{width:200, transform:`rotate(${20}deg)`}} /> */}
+                <img src={logo} style={{width:200, transform:`rotate(${20}deg)`}} />
             </Toolbar>
         </Box>
 	);
