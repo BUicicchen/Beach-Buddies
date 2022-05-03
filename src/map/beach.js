f693677fff6c43e24d7190d7d998e13dcec12feb
Sam Leach <samleach@Sams-MacBook-Pro.local>
2022-05-03 15:19:02 -0400

    certificate data

--- src/map/beach.js
+++ src/map/beach.js
@@ -7,7 +7,7 @@ import BottomNavbar from '../bottomNavbar'
 export default function Beach() {
   const location = useLocation();
   const selectedBeach = location.state.selectedBeach;
-  
+  console.log(selectedBeach)
   return (
     <div style={{textAlign:'center',backgroundColor: "#35559B", height:'calc(206vh)'}}>
         <h1 style={{paddingTop:30}}>{selectedBeach.name}</h1>
